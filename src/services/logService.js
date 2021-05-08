22444d1ce97614858af007c72d4dcf9ce54dbcc0
Jonghyeon Ko <61593290+flowithus@users.noreply.github.com>
2021-05-09 01:28:49 +0900

    Axios, Sentry(logger service) installed

---
+++ src/services/logService.js
@@ -0,0 +1,25 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn:
+      "https://6752913ca83c4480a109365ccb6d914b@o435328.ingest.sentry.io/5755216",
+    release: "1-0-0",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
